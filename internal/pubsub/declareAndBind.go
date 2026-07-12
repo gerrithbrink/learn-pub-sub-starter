ef03fd1b3007ef97b4823d9969fb71970ec9e22b
gerrit <gerritb@eezi.io>
2026-07-12 22:02:52 +0200

    Connect client to RabbitMQ and implement queue binding

---
+++ internal/pubsub/declareAndBind.go
@@ -0,0 +1,39 @@
+package pubsub
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type SimpleQueueType string
+
+const (
+	Durable   SimpleQueueType = "durable"
+	Transient SimpleQueueType = "transient"
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType, // SimpleQueueType is an "enum" type I made to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+
+	ch, err := conn.Channel()
+
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	qu, err := ch.QueueDeclare(queueName, queueType == "durable", queueType == "transient", queueType == "transient", false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	err = ch.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return ch, qu, nil
+
+}

88494527de8b120ae1ea381c67230cce5035ca70
gerrit <gerritb@eezi.io>
2026-07-16 22:05:09 +0200

    Add GOB serialization support for pubsub
    Refactored subscription logic into a generic helper in consume.go and
    added GOB support for both publishing and consuming messages. Deleted
    the now-redundant subscribe.go file.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -49,7 +50,7 @@ func main() {
 		routing.WarRecognitionsPrefix,
 		routing.WarRecognitionsPrefix+".*",
 		pubsub.SimpleQueueDurable,
-		handlerWar(gs),
+		handlerWar(gs, publishCh),
 	)
 	if err != nil {
 		log.Fatalf("could not subscribe to war declarations: %v", err)
@@ -112,64 +113,15 @@ func main() {
 	}
 }
 
-func handlerMove(gs *gamelogic.GameState, publishCh *amqp.Channel) func(gamelogic.ArmyMove) pubsub.Acktype {
-	return func(move gamelogic.ArmyMove) pubsub.Acktype {
-		defer fmt.Print("> ")
-
-		moveOutcome := gs.HandleMove(move)
-		switch moveOutcome {
-		case gamelogic.MoveOutcomeSamePlayer:
-			return pubsub.Ack
-		case gamelogic.MoveOutComeSafe:
-			return pubsub.Ack
-		case gamelogic.MoveOutcomeMakeWar:
-			err := pubsub.PublishJSON(
-				publishCh,
-				routing.ExchangePerilTopic,
-				routing.WarRecognitionsPrefix+"."+gs.GetUsername(),
-				gamelogic.RecognitionOfWar{
-					Attacker: move.Player,
-					Defender: gs.GetPlayerSnap(),
-				},
-			)
-			if err != nil {
-				fmt.Printf("error: %s\n", err)
-				return pubsub.NackRequeue
-			}
-			return pubsub.NackRequeue
-		}
-
-		fmt.Println("error: unknown move outcome")
-		return pubsub.NackDiscard
-	}
-}
-
-func handlerWar(gs *gamelogic.GameState) func(dw gamelogic.RecognitionOfWar) pubsub.Acktype {
-	return func(dw gamelogic.RecognitionOfWar) pubsub.Acktype {
-		defer fmt.Print("> ")
-		warOutcome, _, _ := gs.HandleWar(dw)
-		switch warOutcome {
-		case gamelogic.WarOutcomeNotInvolved:
-			return pubsub.NackRequeue
-		case gamelogic.WarOutcomeNoUnits:
-			return pubsub.NackDiscard
-		case gamelogic.WarOutcomeOpponentWon:
-			return pubsub.Ack
-		case gamelogic.WarOutcomeYouWon:
-			return pubsub.Ack
-		case gamelogic.WarOutcomeDraw:
-			return pubsub.Ack
-		}
-
-		fmt.Println("error: unknown war outcome")
-		return pubsub.NackDiscard
-	}
-}
-
-func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.Acktype {
-	return func(ps routing.PlayingState) pubsub.Acktype {
-		defer fmt.Print("> ")
-		gs.HandlePause(ps)
-		return pubsub.Ack
-	}
+func publishGameLog(publishCh *amqp.Channel, username, msg string) error {
+	return pubsub.PublishGob(
+		publishCh,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug+"."+username,
+		routing.GameLog{
+			Username:    username,
+			CurrentTime: time.Now(),
+			Message:     msg,
+		},
+	)
 }
